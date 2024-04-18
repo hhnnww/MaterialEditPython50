783da19c9f84fec8d425709f1fb02cac3c4658f0
weihua <175990271@qq.com>
2024-04-18 14:30:50 +0800

    12312312312

--- MaterialEdit/fun_制作详情/fun_制作详情.py
+++ MaterialEdit/fun_制作详情/fun_制作详情.py
@@ -2,12 +2,12 @@ from pathlib import Path
 
 from tqdm import tqdm
 
-from .fun_2_构建图片 import fun_构建图片
-from .fun_3_图片编组 import fun_图片编组
-from .fun_4_单行图片制作 import fun_单行图片制作
 from ..fun_图片编辑 import ImageEdit
 from ..setting import MATERIAL_SOURCE_SUFFIX
 from ..type import ALIGNITEM
+from .fun_2_构建图片 import fun_构建图片
+from .fun_3_图片编组 import fun_图片编组
+from .fun_4_单行图片制作 import fun_单行图片制作
 
 
 def fun_制作详情(
@@ -20,7 +20,9 @@ def fun_制作详情(
     xq_width: int = 1500,
 ):
     image_list = fun_构建图片(image_list=image_path_list)
-    comb_image_list = fun_图片编组(image_list, line_number=line_number, max_line_ratio=max_line_ratio)
+    comb_image_list = fun_图片编组(
+        image_list, line_number=line_number, max_line_ratio=max_line_ratio
+    )
 
     material_fil_list = []
     if contains_info is True:
@@ -47,10 +49,18 @@ def fun_制作详情(
         spacing = 180
 
     im = ImageEdit.fun_图片竖向拼接(
-        image_list=comb_pil_list, spacing=spacing, align_item="center", background_color=(255, 255, 255, 255)
+        image_list=comb_pil_list,
+        spacing=spacing,
+        align_item="center",
+        background_color=(255, 255, 255, 255),
     )
     im = ImageEdit.fun_图片扩大粘贴(
-        background_color=(255, 255, 255, 255), height=im.height + 200, width=1500, im=im, left="center", top="top"
+        background_color=(255, 255, 255, 255),
+        height=im.height + 200,
+        width=1500,
+        im=im,
+        left="center",
+        top="start",
     )
 
     return im
