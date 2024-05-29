b15ea83ee84084a25c055f606feccaa1089ffa5b
weihua <175990271@qq.com>
2024-05-29 09:49:04 +0800

    修了了获取素材信息图片列表去掉小图

--- MaterialEdit/fun_文件夹操作/fun_单个文件制作WEB预览图.py
+++ MaterialEdit/fun_文件夹操作/fun_单个文件制作WEB预览图.py
@@ -7,13 +7,13 @@ def fun_单个文件制作WEB预览图(image_path: Path):
     """
     单个图片制作WEB预览图
     """
+    if "_thumb" in image_path.stem:
+        return image_path
+
     new_path = image_path.with_stem(f"{image_path.stem}_thumb")
 
     if new_path.exists() is False:
         im = Image.open(image_path.as_posix())
-        if im.mode != "RGBA":
-            im = im.convert("RGBA")
-
         im.thumbnail((500, 500))
         im.save(new_path.as_posix())
 
--- MaterialEdit/fun_获取素材信息/fun_获取素材信息.py
+++ MaterialEdit/fun_获取素材信息/fun_获取素材信息.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-from .fun_获取所有尺寸 import fun_获取所有尺寸
-from .fun_获取所有文件列表 import fun_获取所有文件列表
-from .fun_获取源文件数量 import fun_获取源文件数量
 from ..fun_创建文件夹结构 import fun_创建文件夹结构
 from ..fun_遍历图片 import fun_遍历图片
 from ..type import MaterialInfo
+from .fun_获取所有尺寸 import fun_获取所有尺寸
+from .fun_获取所有文件列表 import fun_获取所有文件列表
+from .fun_获取源文件数量 import fun_获取源文件数量
 
 
 def fun_获取素材信息(root_path: str, used_image: int, image_sort: bool):
@@ -16,7 +16,11 @@ def fun_获取素材信息(root_path: str, used_image: int, image_sort: bool):
 
     preview_image_path = root_path_structure.preview_path
     preview_image_list = [
-        dict(path=obj) for obj in fun_遍历图片(folder=preview_image_path, image_sort=image_sort, used_image_number=0)
+        dict(path=obj)
+        for obj in fun_遍历图片(
+            folder=preview_image_path, image_sort=image_sort, used_image_number=0
+        )
+        if "_thumb" not in Path(obj).stem
     ]
     preview_image_count = len(preview_image_list)
 
@@ -25,7 +29,11 @@ def fun_获取素材信息(root_path: str, used_image: int, image_sort: bool):
 
     effect_image_path = root_path_structure.effect_path
     effect_image_list = [
-        dict(path=obj) for obj in fun_遍历图片(folder=effect_image_path, image_sort=image_sort, used_image_number=0)
+        dict(path=obj)
+        for obj in fun_遍历图片(
+            folder=effect_image_path, image_sort=image_sort, used_image_number=0
+        )
+        if "_thumb" not in Path(obj).stem
     ]
 
     all_file = fun_获取所有文件列表(root_path_structure.material_path)
@@ -37,7 +45,9 @@ def fun_获取素材信息(root_path: str, used_image: int, image_sort: bool):
 
     material_source_file_count = material_count_num_obj[0][0]
     material_source_format = material_count_num_obj[0][1]
-    material_source_format_number = f"{material_source_file_count}个 {material_source_format} 文件"
+    material_source_format_number = (
+        f"{material_source_file_count}个 {material_source_format} 文件"
+    )
 
     material_source_format_title = ""
     if len(material_count_num_obj) > 0:
