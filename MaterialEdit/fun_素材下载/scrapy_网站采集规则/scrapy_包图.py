bbaebce4aa0fc2c751f54ff04bc6ce39df35b571
weihua <175990271@qq.com>
2024-07-10 14:53:39 +0800

    1231231231

--- MaterialEdit/fun_素材下载/scrapy_网站采集规则/scrapy_包图.py
+++ MaterialEdit/fun_素材下载/scrapy_网站采集规则/scrapy_包图.py
@@ -6,18 +6,20 @@ def scrapy_包图(single_url: str, cookie: str):
     html = fun_session(url=single_url, cookie=cookie)
 
     material_element_list = html.find(
-        r"body > div.page-body.skin-wrap.body-background-gradient > div.bt-body.search.clearfix > "
-        r"div.search-list.box-bg-search.box-bottom-gradient.clearfix > dl"
+        "body > div.page-body.skin-wrap.body-background-gradient > div.bt-body.search.clearfix.search-main > div.search-list.w-search-list.box-bottom-gradient.clearfix > div.result-list.media-list > div > dl > dt"
     )
 
     for obj in material_element_list:
-        if "searchAdver" not in obj.attrs.get("class"):
-            url = "https:" + obj.find("a.jump-details", first=True).attrs.get("href")
+        url = "https:" + obj.find("a.jump-details", first=True).attrs.get("href")
 
-            img_element = obj.find("a.jump-details img", first=True)
-            if img_element is None:
-                return
+        img_element = obj.find("a.jump-details img", first=True)
+        if img_element is None:
+            return
 
-            img_url = "https:" + img_element.attrs.get("data-url")
+        data_url = img_element.attrs.get("data-url")
+        if data_url is None:
+            data_url = img_element.attrs.get("src")
 
-            yield MaterialModel(url=url, img=img_url, state=False)
+        img_url = "https:" + data_url
+
+        yield MaterialModel(url=url, img=img_url, state=False)
